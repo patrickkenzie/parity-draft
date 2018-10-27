94d5b93deb8c4ca484f3d263c957d8d4bf4caf40
Patrick Kenzie <patrickkenzie@gmail.com>
2018-10-26 22:57:06 -0400

    Update player data to use CSV format

--- Players.elm
+++ Players.elm
@@ -1,5 +1,8 @@
 module Players exposing (..)
 
+import Csv exposing (..)
+import Csv.Decode exposing (..)
+
 
 type alias Player =
     { firstName : String
@@ -32,102 +35,122 @@ players =
     sortPlayers .lastName allPlayers
 
 
+playerDecoder : Csv.Decode.Decoder (Player -> a) a
+playerDecoder =
+    map Player
+        (next Result.Ok
+            |> andMap (next Result.Ok)
+            |> andMap (next Result.Ok)
+            |> andMap (next String.toInt)
+        )
+
+
 allPlayers : List Player
 allPlayers =
-    [ { firstName = "Nicholas", lastName = "Aghajanian", gender = "Male", rating = 7 }
-    , { firstName = "Nick", lastName = "Amlin", gender = "Male", rating = 6 }
-    , { firstName = "Craig", lastName = "Anderson", gender = "Male", rating = 8 }
-    , { firstName = "Kevin", lastName = "Barford", gender = "Male", rating = 6 }
-    , { firstName = "Sebastien", lastName = "Belanger", gender = "Male", rating = 6 }
-    , { firstName = "Simon", lastName = "Berry", gender = "Male", rating = 5 }
-    , { firstName = "Shubho Bo", lastName = "Biswas", gender = "Male", rating = 7 }
-    , { firstName = "Lance", lastName = "Blackstock", gender = "Male", rating = 10 }
-    , { firstName = "Ryan", lastName = "Briggs", gender = "Male", rating = 8 }
-    , { firstName = "Graham", lastName = "Brown", gender = "Male", rating = 5 }
-    , { firstName = "Frederic", lastName = "Caron", gender = "Male", rating = 9 }
-    , { firstName = "Christopher", lastName = "Castonguay", gender = "Male", rating = 7 }
-    , { firstName = "Jonathan", lastName = "Champagne", gender = "Male", rating = 5 }
-    , { firstName = "Wing-Leung", lastName = "Chan", gender = "Male", rating = 6 }
-    , { firstName = "Marc", lastName = "Charette", gender = "Male", rating = 4 }
-    , { firstName = "Kelsey", lastName = "Charie", gender = "Male", rating = 7 }
-    , { firstName = "Martin", lastName = "Cloake", gender = "Male", rating = 9 }
-    , { firstName = "Stephen", lastName = "Close", gender = "Male", rating = 7 }
-    , { firstName = "Michael", lastName = "Colantonio", gender = "Male", rating = 6 }
-    , { firstName = "Alessandro", lastName = "Colantonio", gender = "Male", rating = 7 }
-    , { firstName = "Ben", lastName = "Curran", gender = "Male", rating = 5 }
-    , { firstName = "Micheal", lastName = "Davidson", gender = "Male", rating = 3 }
-    , { firstName = "Jason", lastName = "Fraser", gender = "Male", rating = 6 }
-    , { firstName = "Allan", lastName = "Godding", gender = "Male", rating = 6 }
-    , { firstName = "Richard", lastName = "Gregory", gender = "Male", rating = 6 }
-    , { firstName = "John", lastName = "Haig", gender = "Male", rating = 7 }
-    , { firstName = "Scott", lastName = "Higgins", gender = "Male", rating = 7 }
-    , { firstName = "Derek", lastName = "Hodgson", gender = "Male", rating = 8 }
-    , { firstName = "Craig", lastName = "Holden", gender = "Male", rating = 6 }
-    , { firstName = "Jeff", lastName = "Hunt", gender = "Male", rating = 6 }
-    , { firstName = "Rob", lastName = "Ives", gender = "Male", rating = 7 }
-    , { firstName = "Mehmet", lastName = "Karman", gender = "Male", rating = 7 }
-    , { firstName = "Tim", lastName = "Kealey", gender = "Male", rating = 6 }
-    , { firstName = "Chris", lastName = "Keates", gender = "Male", rating = 8 }
-    , { firstName = "Brian", lastName = "Kells", gender = "Male", rating = 8 }
-    , { firstName = "Patrick", lastName = "Kenzie", gender = "Male", rating = 6 }
-    , { firstName = "Nick", lastName = "Klimowicz", gender = "Male", rating = 6 }
-    , { firstName = "Mike", lastName = "Lee", gender = "Male", rating = 10 }
-    , { firstName = "Ken", lastName = "Maclean", gender = "Male", rating = 8 }
-    , { firstName = "Hadrian", lastName = "Mertins-Kirkwood", gender = "Male", rating = 8 }
-    , { firstName = "Tom", lastName = "Newman", gender = "Male", rating = 8 }
-    , { firstName = "David", lastName = "O'Connor", gender = "Male", rating = 6 }
-    , { firstName = "Brian", lastName = "Perry", gender = "Male", rating = 7 }
-    , { firstName = "Jonathan", lastName = "Pindur", gender = "Male", rating = 7 }
-    , { firstName = "Benjamin", lastName = "Piper", gender = "Male", rating = 7 }
-    , { firstName = "Greg", lastName = "Probe", gender = "Male", rating = 7 }
-    , { firstName = "Will", lastName = "Reid", gender = "Male", rating = 6 }
-    , { firstName = "Jim", lastName = "Robinson", gender = "Male", rating = 6 }
-    , { firstName = "Jon", lastName = "Rowe", gender = "Male", rating = 6 }
-    , { firstName = "Thomas", lastName = "Sattolo", gender = "Male", rating = 5 }
-    , { firstName = "Matthew", lastName = "Schijns", gender = "Male", rating = 5 }
-    , { firstName = "Andre", lastName = "Scott", gender = "Male", rating = 5 }
-    , { firstName = "Geofford", lastName = "Seaborn", gender = "Male", rating = 8 }
-    , { firstName = "Andrew", lastName = "Spearin", gender = "Male", rating = 6 }
-    , { firstName = "Trevor", lastName = "Stocki", gender = "Male", rating = 6 }
-    , { firstName = "Chris", lastName = "Sullivan", gender = "Male", rating = 8 }
-    , { firstName = "Nick", lastName = "Theriault", gender = "Male", rating = 6 }
-    , { firstName = "Dan", lastName = "Thomson", gender = "Male", rating = 7 }
-    , { firstName = "David", lastName = "Townsend", gender = "Male", rating = 5 }
-    , { firstName = "Jay Thor", lastName = "Turner", gender = "Male", rating = 6 }
-    , { firstName = "Rob", lastName = "Tyson", gender = "Male", rating = 6 }
-    , { firstName = "Jamie", lastName = "Wildgen", gender = "Male", rating = 7 }
-    , { firstName = "Edwin", lastName = "Wong", gender = "Male", rating = 5 }
-    , { firstName = "Michael", lastName = "Wong", gender = "Male", rating = 5 }
-    , { firstName = "Kate", lastName = "Achtell", gender = "Female", rating = 6 }
-    , { firstName = "Christine", lastName = "Beals", gender = "Female", rating = 5 }
-    , { firstName = "Kristyn", lastName = "Berquist", gender = "Female", rating = 2 }
-    , { firstName = "Cassie", lastName = "Berquist", gender = "Female", rating = 9 }
-    , { firstName = "Jaime", lastName = "Boss", gender = "Female", rating = 8 }
-    , { firstName = "Melany", lastName = "Bouchard", gender = "Female", rating = 8 }
-    , { firstName = "Hope", lastName = "Celani", gender = "Female", rating = 5 }
-    , { firstName = "Laura", lastName = "Chambers Storey", gender = "Female", rating = 7 }
-    , { firstName = "Celine", lastName = "Dumais", gender = "Female", rating = 4 }
-    , { firstName = "Kristie", lastName = "Ellis", gender = "Female", rating = 8 }
-    , { firstName = "Tanya", lastName = "Gallant", gender = "Female", rating = 5 }
-    , { firstName = "Clare", lastName = "Gee", gender = "Female", rating = 5 }
-    , { firstName = "Marie-Ange", lastName = "Gravel", gender = "Female", rating = 5 }
-    , { firstName = "Josee", lastName = "Guibord", gender = "Female", rating = 9 }
-    , { firstName = "Melissa", lastName = "Jess", gender = "Female", rating = 6 }
-    , { firstName = "Adrienne", lastName = "Junek", gender = "Female", rating = 6 }
-    , { firstName = "Ashlin", lastName = "Kelly", gender = "Female", rating = 7 }
-    , { firstName = "Vanessa", lastName = "Mann", gender = "Female", rating = 8 }
-    , { firstName = "Angela", lastName = "Mueller", gender = "Female", rating = 7 }
-    , { firstName = "Rachel", lastName = "Ng", gender = "Female", rating = 3 }
-    , { firstName = "Justine", lastName = "Price", gender = "Female", rating = 7 }
-    , { firstName = "Andrea", lastName = "Proulx", gender = "Female", rating = 8 }
-    , { firstName = "Jessie", lastName = "Robinson", gender = "Female", rating = 8 }
-    , { firstName = "Neena", lastName = "Sidhu", gender = "Female", rating = 6 }
-    , { firstName = "Susan", lastName = "Sunde", gender = "Female", rating = 7 }
-    , { firstName = "An", lastName = "Tran", gender = "Female", rating = 7 }
-    , { firstName = "Stephanie", lastName = "Verbit", gender = "Female", rating = 6 }
-    , { firstName = "Heather", lastName = "Wallace", gender = "Female", rating = 6 }
-    , { firstName = "Michelle", lastName = "Warren", gender = "Female", rating = 9 }
-    , { firstName = "Carrie-Anne", lastName = "Whyte", gender = "Female", rating = 6 }
-    , { firstName = "Stacey", lastName = "Wowchuk", gender = "Female", rating = 6 }
-    , { firstName = "Alisha", lastName = "Zhao", gender = "Female", rating = 10 }
-    ]
+    allPlayersRaw
+        |> Csv.parse
+        |> Csv.Decode.decodeCsv playerDecoder
+        |> Result.withDefault []
+
+
+allPlayersRaw : String
+allPlayersRaw =
+    """
+First Name,Last Name,Gender,Skill Level
+Nicholas,Aghajanian,Male,7
+Nick,Amlin,Male,6
+Craig,Anderson,Male,8
+Kevin,Barford,Male,6
+Sebastien,Belanger,Male,6
+Simon,Berry,Male,5
+Shubho Bo,Biswas,Male,7
+Lance,Blackstock,Male,10
+Ryan,Briggs,Male,8
+Graham,Brown,Male,5
+Frederic,Caron,Male,9
+Christopher,Castonguay,Male,7
+Jonathan,Champagne,Male,5
+Wing-Leung,Chan,Male,6
+Marc,Charette,Male,4
+Kelsey,Charie,Male,7
+Martin,Cloake,Male,9
+Stephen,Close,Male,7
+Michael,Colantonio,Male,6
+Alessandro,Colantonio,Male,7
+Ben,Curran,Male,5
+Micheal,Davidson,Male,3
+Jason,Fraser,Male,6
+Allan,Godding,Male,6
+Richard,Gregory,Male,6
+John,Haig,Male,7
+Scott,Higgins,Male,7
+Derek,Hodgson,Male,8
+Craig,Holden,Male,6
+Jeff,Hunt,Male,6
+Rob,Ives,Male,7
+Mehmet,Karman,Male,7
+Tim,Kealey,Male,6
+Christopher,Keates,Male,8
+Brian,Kells,Male,8
+Patrick,Kenzie,Male,6
+Nick,Klimowicz,Male,6
+Mike,Lee,Male,10
+Ken,Maclean,Male,8
+Hadrian,Mertins-Kirkwood,Male,8
+Tom,Newman,Male,8
+David,O'Connor,Male,6
+Brian,Perry,Male,7
+Jonathan,Pindur,Male,7
+Benjamin,Piper,Male,7
+Greg,Probe,Male,7
+Will,Reid,Male,6
+Jim,Robinson,Male,6
+Jon,Rowe,Male,6
+Thomas,Sattolo,Male,5
+Matthew,Schijns,Male,5
+Andre,Scott,Male,5
+Geofford,Seaborn,Male,8
+Andrew,Spearin,Male,6
+Trevor,Stocki,Male,6
+Chris,Sullivan,Male,8
+Nick,Theriault,Male,6
+Dan,Thomson,Male,7
+David,Townsend,Male,5
+Jay Thor,Turner,Male,6
+Rob,Tyson,Male,6
+Jamie,Wildgen,Male,7
+Edwin,Wong,Male,5
+Michael,Wong,Male,5
+Kate,Achtell,Female,6
+Christine,Beals,Female,5
+Kristyn,Berquist,Female,2
+Cassie,Berquist,Female,9
+Jaime,Boss,Female,8
+Melany,Bouchard,Female,8
+Hope,Celani,Female,5
+Laura,Chambers Storey,Female,7
+Celine,Dumais,Female,4
+Kristie,Ellis,Female,8
+Tanya,Gallant,Female,5
+Clare,Gee,Female,5
+Marie-Ange,Gravel,Female,5
+Josee,Guibord,Female,9
+Melissa,Jess,Female,6
+Adrienne,Junek,Female,6
+Ashlin,Kelly,Female,7
+Vanessa,Mann,Female,8
+Angela,Mueller,Female,7
+Rachel,Ng,Female,3
+Justine,Price,Female,7
+Andrea,Proulx,Female,8
+Jessie,Robinson,Female,8
+Neena,Sidhu,Female,6
+Susan,Sunde,Female,7
+An,Tran,Female,7
+Stephanie,Verbit,Female,6
+Heather,Wallace,Female,6
+Michelle,Warren,Female,9
+Carrie-Anne,Whyte,Female,6
+Stacey,Wowchuk,Female,6
+Alisha,Zhao,Female,10
+"""
